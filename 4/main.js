218e22beba342066d4e36369698b82b5226131d9
Ameer Hamza Khetran <hamzaa803@gmail.com>
2022-11-18 07:51:59 +0500

    Spell

--- 4/main.js
+++ 4/main.js
@@ -3,6 +3,7 @@ Famous Quote: Find a quote from a famous person you admire. Print the quote and 
 
 Albert Einstein once said, “A person who never made a mistake never tried anything new.”
 */
-let quote = '"Knowledge and wisdom are really the privilege of a faithful Muslim. If you have lost them, get them back even though you may have to get them from the apostates."';
+let quote =
+  '"Knowledge and wisdom are really the privilege of a faithful Muslim. If you have lost them, get them back even though you may have to get them from the apostates."';
 let author = "Hazarat Ali";
-console.log(author+" once said, "+quote);
+console.log(author + " once said, " + quote);
